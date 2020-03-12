d41fa23363a96a653360df9eb9b784bcf8003b9c
gilberto-009199 <gilberto.tec@vivaldi.net>
2020-03-12 01:26:24 +0000

    234234

--- Tec.Program.I/litros_de _agua/main.py
+++ Tec.Program.I/litros_de _agua/main.py
@@ -14,4 +14,32 @@ print(" Valor por litro :   ",round(valorPorLitro,3))
 print(" Valor a ser pago :  ",valorConsumido)
 print(" Valor a ser pago com 15% de desconto :  ",round(valorConsumido * 0.85,3))
 
+""" CAP 3 exercicio 5 """
+import math
+# A }
+a = float(input("Digite o valor  de A :"))
+b = float(input("Digite o valor  de B :"))
+x = float(input("Digite o valor  de X :"))
+
+numerador = (a ** 2) +  math.sqrt(3*b)
+divisor   =  5 * math.pow(x,3)
+y         =  numerador / divisor
+
+input(y)
+
+
+""" Uso de fuções trigonometricas """
+
+graus = float(input("iNFORME O VALOR E GRAUS"))
+radian = math.radians(graus)
+
+seno = math.sin(radian)
+cos  = math.cos(radian)
+tan  = math.tan(radian)
+
+print(" Seno :  ",seno)
+print(" Coseno :  ",cos)
+print(" Tangente :  ",tan)
+
+
 
